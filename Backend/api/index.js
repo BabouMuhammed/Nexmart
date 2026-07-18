d9d33a7fcbbbc89c3f31cfb8d3648e57196cb1e0
Muhammed Babou <babou6813@gmail.com>
2026-07-18 17:27:18 +0100

    index.js changes

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -9,6 +9,7 @@ const productRoutes = require('../routes/productRoutes');
 const cartRoutes = require('../routes/cartRoutes');
 const orderRoutes = require('../routes/orderRoutes');
 const reviewRoutes = require('../routes/reviewRoutes');
+const wishlistRoutes = require('../routes/wishlistRoutes');
 const adminRoutes = require('../routes/adminRoutes');
 
 const app = express();
@@ -49,6 +50,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/reviews', reviewRoutes);
+app.use('/api/wishlist', wishlistRoutes);
 app.use('/api/admin', adminRoutes);
 
 app.get('/', (req, res) => {
